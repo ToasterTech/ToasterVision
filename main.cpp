96b6477264cdb6f5aa96132a91adad33f8929aee
Jonathan Daniel <gcagrad19@gmail.com>
2019-01-19 11:40:25 -0500

    Webcam Stream Test
    Tested the webcam stream code, and so far it seems to be working well!

--- main.cpp
+++ main.cpp
@@ -2,23 +2,48 @@
 
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/opencv.hpp"
 #include "ntcore/ntcore_cpp.h"
 #include "ntcore/networktables/NetworkTable.h"
 #include "ntcore/networktables/NetworkTableEntry.h"
 #include "ntcore/networktables/NetworkTableInstance.h"
 
-int main(){	
-	std::cout << "Hello World!\n";
 
-	auto inst = nt::NetworkTableInstance::GetDefault();
-	auto table = inst.GetTable("vision_table");
+int main(){
+	bool DEBUG = true;           //Do we want to show the output screams?
+	bool NETWORK_TABLES = false; //Do we want to send values over Network Tables?
 
-	nt::NetworkTableEntry testEntry = table->GetEntry("TestEntry");
+	std::cout << "Hello World!\n"; //Quick Test Message
+	
+	if(NETWORK_TABLES){ //Set up Network Tables stuff
+		auto inst = nt::NetworkTableInstance::GetDefault();
+		auto table = inst.GetTable("vision_table");
+	
+		nt::NetworkTableEntry testEntry = table->GetEntry("TestEntry");
+	}
 
+	//Camera Setup
+	cv::VideoCapture cap(0);
+	if(!cap.isOpened()){
+		return -1; //Make sure we can open the stream. If not, there is a problem.
+	}
+
+	for(;;){ //Infinite Processing Loop
+		cv::Mat currentFrame;
+		cap >> currentFrame; //get a new frame
+
+		
+
+		if(DEBUG){
+			imshow("Camera", frame);
+		}
+
+		if(cv::waitKey(30) >= 0){
+			break;
+		}
+	}
 	/**
 	for(;;){
 		testEntry.setDouble(5332.0);
 	}**/
-	
-
 }
